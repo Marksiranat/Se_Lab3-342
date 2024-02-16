2631813d16b6d95902556bc4c2f89289d7367474
Marksiranat <siranat2546@gmail.com>
2024-02-16 21:47:45 +0700

    ThaiL

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,7 +9,7 @@
     <q-input
         filled
         v-model="name"
-        label="=ชื่อ  *"
+        label="=ศิระณัฐ จันทะกี"
         hint="ชื่อ-สกุล3"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'เขียนดีๆล่ะ!!']"
